325386fb0d2eece1e9d2f847d54d904ea45ad44c
JhairAgila <jhairagil@gmail.com>
2024-06-21 00:40:52 -0500

    fix/composiing problems

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,5 +1,16 @@
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]']
+# ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]']
 
+ALLOWED_HOSTS = ['*']
+ROOT_URLCONF = 'backend.urls'
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+]
+SECRET_KEY = '6lvbe4y)e*)7*hmf09fz$in8t+8h$xulvdtd=4u9gfagy8%pyc'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
@@ -8,5 +19,31 @@ DATABASES = {
         'PASSWORD': 'ketchup1234',
         'HOST': 'db',
         'PORT': '3306',
+
     }
 }
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,7 +1,14 @@
+# from django.contrib import admin
+# from django.urls import path, include
+
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path('api/', include('app.urls')),
+# ]
+
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('app.urls')),
 ]
