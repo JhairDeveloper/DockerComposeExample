b6592e3911c1071a1bd6c495e9eb991e3a70f830
JhairAgila <jhairagil@gmail.com>
2024-06-28 08:16:06 -0500

    fix/docker-compose

---
+++ usoBD.bash
@@ -0,0 +1,5 @@
+docker exec -it proyecto-db-1
+mariadb -u root -p
+ketchup1234
+USE pract8v2
+INSERT INTO `Academico_docentes` (`codigo`, `nombre`, `materia`, `ciudad`) VALUES ('11', 'Veronica', 'Ingles', 'Loja');
